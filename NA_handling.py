6f6057052ef3613efefd08598c8fc67abb3c96a3
maragori <48766643+maragori@users.noreply.github.com>
2019-04-30 18:24:02 +0200

    update

--- NA_handling.py
+++ NA_handling.py
@@ -14,37 +14,62 @@ for col in data.columns:
 def check_na(feature):
     """
     check correlation of feature with click, bool, position
+    prints the 3 correlations and plots
     :param feature: (str) column name of pandas df
     :return: none
     """
 
     # PROPORTION NAN
     mask = data[feature].notna()
-    len(data[feature].loc[mask]) / len(data[feature])
+    prop_na = len(data[feature].loc[mask]) / len(data[feature])
+    print("Proportion notna values: ", prop_na)
 
 
     # CORRLEATE WITH CLICK, BOOK, POSITION
-    # >>>>>>>>>>>>>>>>>>>> CHECK TYPE OF FEATURE ?? or not
-    #if data[feature].dtype == "float64"
-    book_cor = stats.pointbiserialr(data[feature].loc[mask], data.booking_bool[mask])
-    click_cor = stats.pointbiserialr(data[feature].loc[mask], data.click_bool[mask])
-    pos_cor = stats.pearsonr(data[feature].loc[mask], data.position[mask])
+    # FOR CONTINUOUS FEATURES
+    if data[feature].dtype == "float64":
 
-    print("Correlation with booking_bool: ", book_cor)
-    print("Correlation with click_bool: ", click_cor)
-    print("Correlation with position: ", pos_cor)
+        book_cor = stats.pointbiserialr(data[feature].loc[mask], data.booking_bool[mask])
+        click_cor = stats.pointbiserialr(data[feature].loc[mask], data.click_bool[mask])
+        pos_cor = stats.pearsonr(data[feature].loc[mask], data.position[mask])
+
+        print("Correlation with booking_bool: ", book_cor)
+        print("Correlation with click_bool: ", click_cor)
+        print("Correlation with position: ", pos_cor)
+
+        fig, axs = plt.subplots(1, 3)
+        sns.boxplot(data.click_bool[mask], data[feature].loc[mask], ax=axs[0])
+        axs[0].set_title('click_bool')
+        sns.boxplot(data.booking_bool[mask], data[feature].loc[mask], ax=axs[1])
+        axs[1].set_title('booking_bool')
+        ### SCATTER
+        #sns.scatterplot(data.position[mask], data[feature].loc[mask], s=5, ax=axs[2])
+        sns.regplot(data[feature].loc[mask], data.position[mask], ax=axs[2], marker='o', color='blue', scatter_kws={'s': 2})
+        axs[2].set_title('position')
+
+    # FOR CATEGORICAL FEATURES CODED 0,1
+
+    elif data[feature].dtype == "int64":
+
+        book_cor = stats.pearsonr(data[feature].loc[mask], data.booking_bool[mask])
+        click_cor = stats.pearsonr(data[feature].loc[mask], data.click_bool[mask])
+        pos_cor = stats.pointbiserialr(data[feature].loc[mask], data.position[mask])
+
+        print("Correlation with booking_bool: ", book_cor)
+        print("Correlation with click_bool: ", click_cor)
+        print("Correlation with position: ", pos_cor)
 
 
-    fig, axs = plt.subplots(1, 3)
-    sns.boxplot(data.click_bool[mask], data[feature].loc[mask], ax=axs[0])
-    axs[0].set_title('click_bool')
-    sns.boxplot(data.booking_bool[mask], data[feature].loc[mask], ax=axs[1])
-    axs[1].set_title('booking_bool')
-    ### SCATTER
-    #sns.scatterplot(data.position[mask], data[feature].loc[mask], s=5, ax=axs[2])
-    sns.regplot(data[feature].loc[mask], data.position[mask], marker='o', color='blue', scatter_kws={'s': 2})
-    axs[2].set_title('position')
+        # FIND PLOTS
+        fig, axs = plt.subplots(1, 3)
+        sns.pointplot(data.click_bool[mask], data[feature].loc[mask], ax=axs[0])
+        axs[0].set_title('click_bool')
+        sns.countplot(data.booking_bool[mask], data[feature].loc[mask], ax=axs[1])
+        axs[1].set_title('booking_bool')
+        sns.boxplot(data[feature].loc[mask], data.position[mask], ax=axs[2])
+        axs[2].set_title('position')
 
 
-check_na("prop_location_score2")
+
+check_na("click_bool")
 
