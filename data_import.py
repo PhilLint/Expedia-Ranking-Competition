14055ee7924b0e1a5c5633e267f0d59e9b74a60a
PhilLint <PhilLint@users.noreply.github.com>
2019-05-04 17:36:52 +0200

    oversampling/filtering done

--- data_import.py
+++ data_import.py
@@ -24,17 +24,17 @@ def filter_nothing_instances(data, nothing_ids=None, max_rank=None):
     :return: smaller dataset
     """
     # not booked and not clicked and position smaller than max_rank
-    if max_rank not None:
-        data = data[data['booking_bool'] == 0][data['click_bool'] == 0][data['position']<max_rank]
+    if max_rank is not None:
+        nothing_ids = data[(data['booking_bool'] == 0) & (data['click_bool'] == 0) & (data['position'] < max_rank)].index.values
     # if random choice: choose 0.3 of nothing instances
     else:
         n_sample = np.random.choice(nothing_ids.tolist(), floor(0.3*len(nothing_ids)), False)
-        data = data.loc[n_sample]
+        nothing_ids = data.loc[n_sample].index.values
 
-    return data
+    return nothing_ids
 
 
-def oversample(data, max_rank=None):
+def oversample(data, max_rank=None, print_desc=False):
     """
     Mainly unbooked and unclicked results, therefore to save computational cost and
     weigh relevant instances higher, neither clicked nor booked instances deleted.
@@ -45,15 +45,30 @@ def oversample(data, max_rank=None):
     # booked instances
     book_ids = data[data['booking_bool']==1].index.values
     # only clicked (not booked) instances
-    click_ids = data[data['booking_bool']==0][data['click_bool']==1].index.values
+    click_ids = data[(data['booking_bool']==0) & (data['click_bool']==1)].index.values
     # neither clicked nor booked
-    nothing_ids = data[data['booking_bool']==0][data['click_bool']==0].index.values
+    nothing_ids = data[(data['booking_bool']==0) & (data['click_bool']==0)].index.values
     # number booked
-    book_cutoff = book_ids.size
+    number_book= book_ids.size
     # number clicked
     number_clicks = click_ids.size
+    # only keep books and clicks
     # filter nothing instances down either randomly or only keep nothing instances
     # of position smaller than max_rank
-    data = filter_nothing_instances(data, nothing_ids=nothing_ids, max_rank=max_rank)
-    return data, book_cutoff,
+    filtered_nothing_ids = filter_nothing_instances(data, nothing_ids=nothing_ids, max_rank=max_rank)
+    # filter out the dataset
+    data = pd.concat([data[data.index.isin(book_ids)], data[data.index.isin(click_ids)],  data[data.index.isin(filtered_nothing_ids)]])
+    # print descriptives
+    if print_desc:
+        print("Number of observations: " + str(len(new_training)) + " ||  number of bookings: " + str(number_books) +
+              " ||  number of clicks: " + str(number_clicks))
+    return data, number_book, number_clicks
 
+new_training, number_books, number_clicks = oversample(training)
+# plot poisition to frequency booked (1) or not booked (0)
+new_training['position'].hist(by=new_training['booking_bool'])
+plt.show()
+# now with max_rank
+new_training, number_books, number_clicks = oversample(training, 15)
+new_training['position'].hist(by=new_training['booking_bool'])
+plt.show()
