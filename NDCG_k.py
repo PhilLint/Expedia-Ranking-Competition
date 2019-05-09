494be712bb7b033bba588167dc634cbf8180bbb9
maragori <48766643+maragori@users.noreply.github.com>
2019-05-09 07:25:17 +0200

    finished calculate_score function changed first line of submission sample to contain a correct prediction for srch_id 1 from the dataset

--- NDCG_k.py
+++ NDCG_k.py
@@ -45,32 +45,39 @@ def ndcg_at_k(r, k):
 
 def calculate_score(submission, y_test):
     """
-    NOT FINISHED YET -
-    returns total ndcg score for a submission in the required format
+    returns average ndcg score over all queries for a submission in the required format
     :param submission: submission of ranked hotel properties given a srch_id
     :param y_test: test data
     :return: score and altered submission df
     """
+
     # for each row of the prediction, check whether there exists a query in y_test that has a prop_id that is either
     # booked/clicked/nothing and create new df column with corresponding value 5/1/0
+
     submission["score"] = [5 if y_test.loc[((y_test["srch_id"] == srch_id) &
                                             (y_test["prop_id"] == prop_id)), "booking_bool"].any() else
                            1 if y_test.loc[((y_test["srch_id"] == srch_id) &
                                             (y_test["prop_id"] == prop_id)), "click_bool"].any() else 0
                             for (srch_id, prop_id) in list(zip(submission.srch_id, submission.prop_id))]
 
-    # to come: given the new submission format, calculate the ndcg for the submission
+    # given the new submission format, calculate the ndcg for the submission
 
-    return submission
+    grouped_sub = submission.groupby(["srch_id"])["score"]
+    score = 0
+    for _, val in grouped_sub:
+        score += ndcg_at_k(val.to_list(), 5)
+
+    score /= len(grouped_sub)
+    return score
+
+
+
+
 
 
 if __name__ == "__main__":
-    # TEST
-    #r = [1,0,5,0,0]
-    #result_ndcg = ndcg_at_k(r=r, k=5)
-    #print(result_ndcg)
-    sub = pd.read_csv("submission_sample.csv", nrows=10_000)
 
-    y_test = pd.read_csv("C:/Users/Frede/Dropbox/Master/DM/Assignments/2/DM2/training_set_VU_DM.csv", nrows=10_000)
+    sub = pd.read_csv("submission_sample.csv", nrows=50)
+    y_test = pd.read_csv("C:/Users/Frede/Dropbox/Master/DM/Assignments/2/DM2/training_set_VU_DM.csv", nrows=50)
 
     print(calculate_score(sub, y_test))
