0a96e7b1f81293802f1393d83b16104e05e4d054
maragori <48766643+maragori@users.noreply.github.com>
2019-05-18 13:19:14 +0200

    scoring function completed

--- feature_selection.py
+++ feature_selection.py
@@ -1,22 +1,36 @@
-from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+import pandas as pd
+from sklearn.feature_selection import RFE
+from sklearn.ensemble import RandomForestRegressor
+from data_import import oversample
 
 
-def forest_feat_select(X, y, n_estimators):
-    """
-    given X and y determine the importance of each feature in X in predicting y
-    by applying random a forrest classifier
-    :param X: pandas df
-    :param y: single column pandas df or 1d numpy array
-    :param n_estimators: n_estimators arg passed to rf_classifier instance
-    :return:
-    """
 
-    rf = RandomForestClassifier(n_estimators=100)
+def feature_selection(estimator, n_features=None):
 
-    rf.fit(X, y)
-    feature_importances = rf.feature_importances_
+    eleminator = RFE(estimator=estimator, n_features_to_select=n_features)
 
-    print("feature importance")
-    for pair in zip(X.columns, feature_importances):
-        print(pair)
+
+
+
+if __name__ == "main":
+    data = pd.read_csv("C:/Users/Frede/Dropbox/Master/DM/Assignments/2/DM2/training_set_VU_DM.csv", nrows=100_000)
+    data = data.loc[:, data.columns != "date_time"]
+    data_no_na = data.loc[:, data.notnull().all()]
+
+    data_down, _, _, _ = oversample(data_no_na, max_rank=5)
+
+    X_train = data_down.drop(columns=["booking_bool", "click_bool", "position"])
+    y_train = data_down.loc[:, "booking_bool"]
+
+    estimator = RandomForestRegressor(n_estimators=100)
+    selector = RFE(estimator=estimator)
+    selector.fit(X_train, y_train)
+
+    cols = selector.support_
+
+
+
+
+
 
--- scoring.py
+++ scoring.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 
+# constant k for ndcg
+K = 5
+
 def dcg_at_k(r, k):
     """Score is discounted cumulative gain (dcg)
     Relevance is positive real values.  Can use binary
@@ -42,11 +45,8 @@ def ndcg_at_k(r, k):
         return 0.
     return dcg_at_k(r, k) / dcg_max
 
-r = [0,0,0,0,0,0,0,0]
-print(dcg_at_k(r,5))
-print(ndcg_at_k(r,5))
 
-def calculate_score(submission, y_test):
+def calculate_score(submission, y_test, k=K):
     """
     RUNTIME 100_000 rows: approx. 5 mins
 
@@ -59,6 +59,7 @@ def calculate_score(submission, y_test):
     # for each row of the prediction, check whether there exists a query in y_test that has a prop_id that is either
     # booked/clicked/nothing and create new df column with corresponding value 5/1/0
 
+    print("Matching true booking_bool/click_bool values to submission...\n")
     submission["score"] = [5 if y_test.loc[((y_test["srch_id"] == srch_id) &
                                             (y_test["prop_id"] == prop_id)), "booking_bool"].any() else
                            1 if y_test.loc[((y_test["srch_id"] == srch_id) &
@@ -67,13 +68,49 @@ def calculate_score(submission, y_test):
 
     # given the new submission format, calculate the ndcg for the submission
 
+    print(f"Calculating NDCG_{k} score...\n")
     grouped_sub = submission.groupby(["srch_id"])["score"]
     score = 0
     for _, val in grouped_sub:
-        score += ndcg_at_k(val.to_list(), 5)
+        score += ndcg_at_k(val.to_list(), k)
 
     score /= len(grouped_sub)
     return score
+
+
+
+
+def prediction_to_submission(prediction, y_test):
+    """
+    convert from single column pandas df with predicted booking_bool values into sorted submission file
+    :param prediction: single column pandas df
+    :param y_test: test_set: required columns: srch_id, prop_id
+    :return: two column pandas df with srch_id, prop_id
+    """
+
+    y_test["prediction"] = prediction
+    y_test_sorted = y_test.sort_values(["srch_id", "prediction"], ascending=[True, False])
+    return y_test_sorted[["srch_id", "prop_id"]]
+
+
+def score_prediction(prediction, y_test, k=K, to_print=True):
+    """
+    Convert prediction into submission format and calculate ndcg score
+    :param prediction: single column pandas df
+    :param y_test: test_set: required columns: srch_id, prop_id
+    :param k: ndcg parameter
+    :param to_print: if True, score is printed, otherwise returned
+    :return: ndcg score if to_print = False
+    """
+
+    print("Converting into submission format...\n")
+    submission = prediction_to_submission(prediction, y_test)
+    print("Calling scoring function...\n")
+    score = calculate_score(submission, y_test, k)
+    if to_print:
+        print(f"NDCG_{k} prediction score for this prediction: {score}.\n")
+    else:
+        return score
 
 
 if __name__ == "__main__":
@@ -81,4 +118,4 @@ if __name__ == "__main__":
     sub = pd.read_csv("submission_sample.csv", nrows=64)
     y_test = pd.read_csv("training_set_VU_DM.csv", nrows=64)
 
-    print(calculate_score(sub, y_test))
+
